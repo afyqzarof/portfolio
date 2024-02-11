e9597207a3aef788bc6bcbf7d65836d632eabc03
afyqeyzar <norafyqe@gmail.com>
2024-02-11 20:56:39 +0000

    feat: move nav to side

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,46 +14,44 @@ const Header = () => {
     setViewport({ x, y, zoom }, { duration });
   };
   return (
-    <header className="header">
-      <nav className="nav">
-        <button
-          type="button"
-          className="nav__btn"
-          onClick={() => {
-            if (screenWidth <= 425) {
-              handleTransform(20, 100, 1, 800);
-              return;
-            }
-            handleTransform(300, 200, 3, 800);
-          }}>
-          home
-        </button>
-        <button
-          type="button"
-          className="nav__btn"
-          onClick={() => {
-            handleTransform(-200, -100, 1, 1000);
-          }}>
-          projects
-        </button>
-        <button
-          type="button"
-          className="nav__btn"
-          onClick={() => {
-            handleTransform(1600, -400, 1, 1000);
-          }}>
-          about
-        </button>
-        <button
-          type="button"
-          className="nav__btn"
-          onClick={() => {
-            handleTransform(1200, 850, 1.5, 1000);
-          }}>
-          contact
-        </button>
-      </nav>
-    </header>
+    <nav className="nav">
+      <button
+        type="button"
+        className="nav__btn"
+        onClick={() => {
+          if (screenWidth <= 425) {
+            handleTransform(20, 100, 1, 800);
+            return;
+          }
+          handleTransform(300, 200, 3, 800);
+        }}>
+        home
+      </button>
+      <button
+        type="button"
+        className="nav__btn"
+        onClick={() => {
+          handleTransform(-200, -100, 1, 1000);
+        }}>
+        projects
+      </button>
+      <button
+        type="button"
+        className="nav__btn"
+        onClick={() => {
+          handleTransform(1600, -400, 1, 1000);
+        }}>
+        about
+      </button>
+      <button
+        type="button"
+        className="nav__btn"
+        onClick={() => {
+          handleTransform(1200, 850, 1.5, 1000);
+        }}>
+        contact
+      </button>
+    </nav>
   );
 };
 
